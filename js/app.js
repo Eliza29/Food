85ac3a900f5e974627bc256556fda656f00ebe13
eliza29 <elizaseguraf@gmail.com>
2017-12-30 14:47:39 -0500

    subiendo imagenes

--- js/app.js
+++ js/app.js
@@ -1,10 +1,9 @@
-/* Funcionalidades */
+/* Funcionalidades 
 $(document).ready(function() {
   setTimeout(function() { 
     window.location.href = 'views/district.html';
   }, 5000); 
   
    
-});
-
+*/
 //
